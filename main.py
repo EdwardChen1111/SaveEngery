99b17a7dccc6af17e05f7e2a6736ed11cf3b9bde
EdwardChen1111 <asus960324@gmail.com>
2023-08-09 03:52:10 +0800

    trainspawn80%

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import numpy as np
 import pyaudio
 import time
 import wave
+from tkinter import ttk
+from tkinter import messagebox
 
 fig, ax = plt.subplots()
 now = ""
+watertype = ""
+soundnum = 0
 
 def runStartGUI():
     global now
@@ -15,77 +19,88 @@ def runStartGUI():
     ans = ""
     
     def startGUI():
-        def btn(msg):
+        def startb():
+            global watertype, soundnum
             nonlocal ans
-            ans = msg
-                
+            if now != "listen":
+                ans, soundnum = "start", int(soundent.get())
+                if watercombo.get() == '錄製正常水流聲':
+                    watertype = 0
+                else:
+                    watertype = 1
+            
         def exit():
-            btn("exit")
+            nonlocal ans
+            ans = "exit"
             start.destroy()
         
         start = tk.Tk()
         start.title("startlist")
-        start.geometry("360x60")
-        startbtn = tk.Button(start, width=10, height=2, text="開始偵測", command=lambda:btn("start")).grid(row=0,column=0)
-        endbtn = tk.Button(start, width=10, height=2, text="結束偵測", command=lambda:btn("end")).grid(row=0,column=1)
-        exitbtn = tk.Button(start, width=10, height=2, text="結束程式", command=exit).grid(row=0,column=2)
+        start.geometry("240x130")
+        watercombo = ttk.Combobox(start, width=20, height=2, values=['錄製正常水流聲','錄製漏水水流聲'])
+        soundent = tk.Entry(start, width=20)
+        startbtn = tk.Button(start, width=10, height=2, text="開始偵測", command=startb)
+        exitbtn = tk.Button(start, width=10, height=2, text="結束程式", command=exit)
+        watercombo.grid(row=0,columnspan=2)
+        soundent.grid(row=1,column=0,columnspan=2)
+        startbtn.grid(row=2,column=0,pady=20)
+        exitbtn.grid(row=2,column=1,pady=20)
+        
         start.mainloop()
     
     GUI = thread.Thread(target=startGUI)
     GUI.start()
     
-    while ans != "exit":
+    while True:
         time.sleep(0.1)
         if ans == "start":
             startListen = thread.Thread(target=startListening)
             startListen.start()
             now = "listen"
             ans = ""
-        elif ans == "end":
+        elif ans == "exit":
             now = "end"
-            ans = ""
-            
-    now = "end"
-    return 0
+            break
 
 def startListening():
-    global now
+    global now, watertype, soundnum
+    
     p = pyaudio.PyAudio()
-    chunk = 1024
+    chunk = 44100
     sample_format = pyaudio.paInt16
-    channels = 2
+    channels = 1
     fs = 44100
-    filename = "oxxostudio.wav"
     stream = p.open(format=sample_format,
                     channels=channels,
                     rate=fs,
                     input=True,
                     frames_per_buffer=chunk)
-    frames = []
-    while now != "end":
-        data = stream.read(chunk)
-        frames.append(data)
-        if len(frames) > 3400:
-            frames.pop(0)
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
-def visualize(path):
-    raw = wave.open(path)
-    signal = raw.readframes(-1)
-    signal = np.frombuffer(signal, dtype ="int16")
-    f_rate = raw.getframerate()
-    time = np.linspace(0, len(signal)/f_rate, num = len(signal))
-
-    ax.plot(time, signal)
-    plt.title("Sound Wave")
-    plt.xlabel("Time")
-    plt.show()
+    soundtemp = np.load("train.npz")
+    soundarr, soundlab = soundtemp['x_train'], soundtemp['x_labels']
+    soundarr = soundarr.tolist()
+    soundlab = soundlab.tolist()
+    
+    for i in range(soundnum):
+        if now != "end":
+            frames = []
+            for j in range(5):
+                data = stream.read(chunk)
+                frames += data
+            for k in range(len(frames)):
+                frames[k] /= 65535
+            
+            soundarr.append(frames)
+            soundlab.append(watertype)
+            print(len(frames), len(soundarr))
+        else:
+            break
+    stream.stop_stream()
+    stream.close()
+    soundarr = np.array(soundarr)
+    soundlab = np.array(soundlab)    
+    
+    np.savez_compressed("train.npz", x_train=soundarr, x_labels=soundlab)
+    now = "starting"
 
 if __name__ == '__main__':
     runStartGUI()
-    visualize("oxxostudio.wav")
---
+++ npzcreate.py
@@ -0,0 +1,2 @@
+import numpy as np
+np.savez_compressed("train.npz", x_train=np.array([]), x_labels=np.array([]))
