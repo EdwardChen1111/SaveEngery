ddc0f6bfd937daf46ab37f80ab8b6f3c3263b470
EdwardChen1111 <asus960324@gmail.com>
2023-08-21 23:22:47 +0800

    change

--- runModel/runModel.py
+++ runModel/runModel.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
-import numpy as np
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind(('chatbotpersonal.ddns.net', 5555))
-#here in the video they used 127.0.0.1 as it is for localhost
 
 s.listen()
 
@@ -14,18 +12,13 @@ def squeeze(audio, labels):
   return audio, labels
 
 def get_spectrogram(waveform):
-  # Convert the waveform to a spectrogram via a STFT.
   spectrogram = tf.signal.stft(
       waveform, frame_length=255, frame_step=128)
-  # Obtain the magnitude of the STFT.
   spectrogram = tf.abs(spectrogram)
-  # Add a `channels` dimension, so that the spectrogram can be used
-  # as image-like input data with convolution layers (which expect
-  # shape (`batch_size`, `height`, `width`, `channels`).
   spectrogram = spectrogram[..., tf.newaxis]
   return spectrogram
 
-model = tf.keras.models.load_model('saved_model/my_model')
+model = tf.keras.models.load_model('saved_model')
 
 while True:
     client, address = s.accept()
